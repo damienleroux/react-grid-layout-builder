33930d58e4af97375a4660f1c8110350b587a1ca
nywooz <nywooz7@gmail.com>
2019-03-21 21:06:57 +0000

    Update demo/src/file-download.js to use es6

--- demo/src/file-download.js
+++ demo/src/file-download.js
@@ -1,5 +1,5 @@
 const fileDownload = (data, filename, mime) => {
-  var blob = new Blob([data], { type: mime || "application/octet-stream" });
+  const blob = new Blob([data], { type: mime || "application/octet-stream" });
   if (typeof window.navigator.msSaveBlob !== "undefined") {
     // IE workaround for "HTML7007: One or more blob URLs were
     // revoked by closing the blob for which they were created.
@@ -7,8 +7,8 @@ const fileDownload = (data, filename, mime) => {
     // the URL has been freed."
     window.navigator.msSaveBlob(blob, filename);
   } else {
-    var blobURL = window.URL.createObjectURL(blob);
-    var tempLink = document.createElement("a");
+    const blobURL = window.URL.createObjectURL(blob);
+    const tempLink = document.createElement("a");
     tempLink.style.display = "none";
     tempLink.href = blobURL;
     tempLink.setAttribute("download", filename);
